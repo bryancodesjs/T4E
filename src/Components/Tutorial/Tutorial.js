97883a48cceb3183c5b1baee82f0a8b5e2b55443
Martin Perez <josueceeu@gmail.com>
2021-04-25 17:27:10 -0400

    expanded tutorial section

--- src/Components/Tutorial/Tutorial.js
+++ src/Components/Tutorial/Tutorial.js
@@ -5,14 +5,33 @@ import post from './post.png'
 import linkImg from './link.png'
 import reg1 from './reg1.jpg'
 import reg2 from './reg2.png'
+import reg3 from './reg3.jpg'
+import directCommission from './directCommission.jpg'
+import teamCommission from './teamCommission.png'
 import directLicense from './direct-license-tutorial-img.png'
 import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
+function stepTwo() {
+    document.getElementById('step1').classList.remove('active');
+    document.getElementById('step2').classList.add('active');
+    document.getElementById('scrollTarget').scrollIntoView(); //This ensures the viewport is scrolled top the top of the section
+}
+function stepThree() {
+    document.getElementById('step2').classList.remove('active');
+    document.getElementById('step3').classList.add('active');
+    document.getElementById('scrollTarget').scrollIntoView();
+}
+function stepFour() {
+    document.getElementById('step3').classList.remove('active');
+    document.getElementById('step4').classList.add('active');
+    document.getElementById('scrollTarget').scrollIntoView();
+}
 function Tutoril() {
     return (
         <div class="" id="backofficewrap">
             <MyNav />
             <Banner />
+            <div id="scrollTarget"></div>
             <div class="container">
                 <div class="row ">
                     <div class="xwrap mt-5 p-4">
@@ -50,9 +69,8 @@ function Tutoril() {
                                 </div>
                             </div>
 
-                            <div className="d-flex justify-content-between flex-wrap mb-5">
-                                <button className="btn btn-outline-primary"><FaChevronLeft /> Home Page</button>
-                                <button className="btn btn-primary oge">Registration <FaChevronRight /></button>
+                            <div className="d-flex justify-content-end flex-wrap mb-5">
+                                <button className="btn btn-primary oge" onClick={() => stepTwo()}>Registration <FaChevronRight /></button>
                             </div>
 
                         </div>
@@ -64,27 +82,101 @@ function Tutoril() {
                             </p>
 
                             <div className="d-flex justify-content-between flex-wrap mb-5">
-                                <div className="col-lg-4 flex-column align-items-center d-flex p-4">
-                                    <img className="reg-img shadow mb-2" src={reg1} alt="share, earn, repeat with tron alliance" />
+                                <div className="col-lg-4 flex-column align-items-center d-flex p-4 text-center">
+                                    <img className="reg-img shadow mb-4" src={reg1} alt="share, earn, repeat with tron alliance" />
                                     <p>Navigate to the registration page and enter your sponsor's ID.</p>
                                 </div>
 
-                                <div className="col-lg-4 flex-column align-items-center d-flex p-4">
-                                    <img className="reg-img shadow mb-2" src={reg2} alt="share, earn, repeat with tron alliance" />
+                                <div className="col-lg-4 flex-column align-items-center d-flex p-4 text-center">
+                                    <img className="reg-img shadow mb-4" src={reg2} alt="share, earn, repeat with tron alliance" />
                                     <p>Your wallet will be triggered. Approve your registration payment.</p>
                                 </div>
 
-                                <div className="col-lg-4 flex-column align-items-center d-flex p-4">
-                                    <img className="direct-license-img mb-2" src={post} alt="share, earn, repeat with tron alliance" />
-                                    <p>Lorem Ipsum Dolor Sit Amet</p>
+                                <div className="col-lg-4 flex-column align-items-center d-flex p-4 text-center">
+                                    <img className="reg-img shadow mb-4" src={reg3} alt="share, earn, repeat with tron alliance" />
+                                    <p>Your account is ready and know you can start referring and earning.</p>
                                 </div>
                             </div>
 
                             <div className="d-flex justify-content-between flex-wrap mb-5">
                                 <button className="btn btn-outline-primary"><FaChevronLeft /> How it works</button>
-                                <button className="btn btn-primary oge">How to make money <FaChevronRight /></button>
+                                <button className="btn btn-primary oge" onClick={() => stepThree()}>How to make money <FaChevronRight /></button>
                             </div>
 
+                        </div>
+
+                        <div id="step3" class="tutorial-section">
+                            <h1 class=""><strong>How to make money</strong></h1>
+                            <p class="whitext">
+                                You make money by promoting our Affiliate Licenses. When your account is created you receive two licenses: 
+                            </p>
+                            <div className="d-flex justify-content-between flex-wrap mb-5">
+                                <div className="col-lg-4 flex-column align-items-center d-flex">
+                                    <h2 className=""><strong><span className="accent">1.</span> Direct License</strong></h2>
+                                    <img className="direct-license-img" src={directCommission} alt="share, earn, repeat with tron alliance" />
+                                    <p>
+                                    Everytime a new user registers using your <i>Affiliate Link</i> or your user ID, you earn a 100% commission 
+                                    based on the value of the license.
+                                    </p>
+                                    <h4>A 200TRX direct license gives you 200TRX direct commission.</h4>
+                                </div>
+                                <div className="col-lg-4 flex-column align-items-center d-flex">
+                                    <h2 className=""><strong><span className="accent">2.</span> Team License</strong></h2>
+                                    <img className="direct-license-img" src={teamCommission} alt="share, earn, repeat with tron alliance" />
+                                    <p>
+                                    Everytime anyone in your <span title="Someone you have directly referred using your ID">team</span> refers a new user, 
+                                    you instantly get a 20% commission based on the value of the license.
+                                    </p>
+                                    <h4>A 200TRX team license gives you a 40TRX team commission.</h4>
+                                </div>
+                            </div>
+                            <div className="alert alert-success mb-5" role="alert">
+                                <p class="whitext">
+                                    All commissions you earn are instantly deposited to your TRX wallet. Tron Alliance is a smart contract and as such, 
+                                    all the money is instantly distributed to the users.
+                                </p>
+                            </div>
+                            <div className="d-flex justify-content-between flex-wrap mb-5">
+                                <button className="btn btn-outline-primary"><FaChevronLeft /> Registration</button>
+                                <button className="btn btn-primary oge" onClick={() => stepFour()}>Boost your earnings <FaChevronRight /></button>
+                            </div>
+                        </div>
+
+                        <div id="step4" class="tutorial-section">
+                            <h1 class=""><strong>Boost your earnings</strong></h1>
+                            <p class="whitext">
+                                There's no limit to how much you can earn. 
+                            </p>
+                            <div className="d-flex justify-content-between flex-wrap mb-5">
+                                <div className="col-lg-4 flex-column align-items-center d-flex">
+                                    <h2 className=""><strong><span className="accent">1.</span> Direct License</strong></h2>
+                                    <img className="direct-license-img" src={directCommission} alt="share, earn, repeat with tron alliance" />
+                                    <p>
+                                    Everytime a new user registers using your <i>Affiliate Link</i> or your user ID, you earn a 100% commission 
+                                    based on the value of the license.
+                                    </p>
+                                    <h4>A 200TRX direct license gives you 200TRX direct commission.</h4>
+                                </div>
+                                <div className="col-lg-4 flex-column align-items-center d-flex">
+                                    <h2 className=""><strong><span className="accent">2.</span> Team License</strong></h2>
+                                    <img className="direct-license-img" src={teamCommission} alt="share, earn, repeat with tron alliance" />
+                                    <p>
+                                    Everytime anyone in your <span title="Someone you have directly referred using your ID">team</span> refers a new user, 
+                                    you instantly get a 20% commission based on the value of the license.
+                                    </p>
+                                    <h4>A 200TRX team license gives you a 40TRX team commission.</h4>
+                                </div>
+                            </div>
+                            <div className="alert alert-success mb-5" role="alert">
+                                <p class="whitext">
+                                    All commissions you earn are instantly deposited to your TRX wallet. Tron Alliance is a smart contract and as such, 
+                                    all the money is instantly distributed to the users.
+                                </p>
+                            </div>
+                            <div className="d-flex justify-content-between flex-wrap mb-5">
+                                <button className="btn btn-outline-primary"><FaChevronLeft /> Registration</button>
+                                <button className="btn btn-primary oge" onClick={() => stepTwo()}>Boost your earnings <FaChevronRight /></button>
+                            </div>
                         </div>
                     </div>
                 </div>
